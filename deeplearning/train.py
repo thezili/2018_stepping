06487a019e027417c55047ee894ea955c23d6434
Soojin <thezili.soojin@gamil.com>
2018-05-11 21:02:51 +0900

    modify train.py

--- deeplearning/train.py
+++ deeplearning/train.py
@@ -1,34 +1,67 @@
-import torch.utils.data as data
-import torch.optim as optim
-from torch.autograd import Variable
-import torch.optim.lr_scheduler as lr_scheduler
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Author:   Kazuto Nakashima
+# URL:      http://kazuto1011.github.io
+# Created:  2017-11-01
+
+from __future__ import absolute_import, division, print_function
+
+import os.path as osp
+
+import click
+import cv2
+import numpy as np
+import torch
 import torch.nn as nn
-import glob
+import torch.nn.functional as F
+from addict import Dict
+from tensorboardX import SummaryWriter
+# from torch.autograd import Variable
+#from torchnet.meter import MovingAverageValueMeter
 from tqdm import tqdm
-import os
-from PIL import Image
-from utils.dataset import GIANA
-import utils.transforms as t
-import utils.co_transforms as cot
+import yaml
 from models.deeplabv3 import DeepLabV3
-# from models.unet import (UNet, UNetRes)
-# from models.fcn import (FCN32, FCN16, FCN8)
-# from models.pspnet import PSPNet
-# from models.segnet import SegNet
-# from models.gcn import ResnetGCN
-# from models.fusionnet import FusionNet
-# from models.linknet import LinkNet
-# from models.duc import (ResentDUC, ResentDUCHDC)
-# from models.discriminator import Discriminator
-from utils.avg import AverageMeter
-from utils.evaluation import Evaluation
-# from utils.visualize import Dashboad
-from utils.losses import *
-from utils.experiments import Experiments
-from utils.tools import morph_postprocess
-from settings import get_arguments
-import sys
-import itertools
+from utils.loss import CrossEntropyLoss2d
+import utils.transforms as t
+from utils.dataset import GIANA
+
+def get_lr_params(model, key):
+    # For Dilated FCN
+    if key == '1x':
+        for m in model.named_modules():
+            if 'layer' in m[0]:
+                if isinstance(m[1], nn.Conv2d):
+                    for p in m[1].parameters():
+                        yield p
+    # For conv weight in the ASPP module
+    if key == '10x':
+        for m in model.named_modules():
+            if 'aspp' in m[0]:
+                if isinstance(m[1], nn.Conv2d):
+                    yield m[1].weight
+    # For conv bias in the ASPP module
+    if key == '20x':
+        for m in model.named_modules():
+            if 'aspp' in m[0]:
+                if isinstance(m[1], nn.Conv2d):
+                    yield m[1].bias
+
+
+def poly_lr_scheduler(optimizer, init_lr, iter, lr_decay_iter, max_iter, power):
+    if iter % lr_decay_iter or iter > max_iter:
+        return None
+    new_lr = init_lr * (1 - float(iter) / max_iter)**power
+    optimizer.param_groups[0]['lr'] = new_lr
+    optimizer.param_groups[1]['lr'] = 10 * new_lr
+    optimizer.param_groups[2]['lr'] = 20 * new_lr
+
+
+def resize_target(target, size):
+    new_target = np.zeros((target.shape[0], size, size), np.int32)
+    for i, t in enumerate(target.numpy()):
+        new_target[i, ...] = cv2.resize(t, (size, ) * 2, interpolation=cv2.INTER_NEAREST)
+    return torch.from_numpy(new_target).long()
 
 def load_data(args):
 
@@ -37,8 +70,7 @@ def load_data(args):
 
     # Use  the following code fo co_transformations e.g. random rotation or random flip etc.
     # co_transformer = cot.Compose([cot.RandomRotate(45)])
-
-    dsetTrain = GIANA(args.imgdir, args.gtdir, input_size=(args.input_width, args.input_height) ,train=True, transform=im_transform, co_transform=None, target_transform=t.ToLabel())
+    dsetTrain = GIANA(args.imgdir, args.gtdir, input_size=(args.input_width, args.input_height) ,train=True, transform=im_transform, co_transform=None, target_transform=t.ToLabel(), train_split=0.8)
     train_data_loader = data.DataLoader(dsetTrain, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
     dsetVal = GIANA(args.imgdir, args.gtdir, train=False, transform=im_transform, co_transform=None,
@@ -48,192 +80,181 @@ def load_data(args):
     return train_data_loader, val_data_loader
 
 
-def train(args, model):
-    # board = Dashboad(args.visdom_port)
-    tr_losses = AverageMeter()
-    tLoader, vLoader = load_data(args)
+#@click.command()
+#@click.option('--config', '-c', type=str, required=True)
+#@click.option('--cuda/--no-cuda', default=True)
+def main():
+    # Configuration
+    CONFIG = Dict(yaml.load(open('/home/modulabs/soojin/2018_stepping/deeplearning/giana.yaml')))
 
-    criterion = nn.BCELoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=0.99)
-    scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    # CUDA check
+    cuda = torch.cuda.is_available()
+    if cuda:
+        current_device = torch.cuda.current_device()
+        print('Running on', torch.cuda.get_device_name(current_device))
 
-    for epoch in range(1, args.num_epochs+1):
-        scheduler.step()
-        if epoch == 1:
-            tr_loss, _ , _ = evaluate(args, model, criterion, tLoader)
-            vl_loss, vl_jacc, vl_dice = evaluate(args, model, criterion, vLoader)
+    # Dataset
+    # dataset = CocoStuff10k(
+    #     root=CONFIG.ROOT,
+    #     split='train',
+    #     image_size=513,
+    #     crop_size=CONFIG.IMAGE.SIZE.TRAIN,
+    #     scale=True,
+    #     flip=True,
+    # )
 
-            # Draw the loss curves
-            win = None
-            # win = board.loss_curves([tr_loss, vl_loss], epoch, win=win)
-            print('[Initial TrainLoss: {0:.4f}]'
-                  '\t[Initial ValidationLoss: {1:.4f}]'
-                  '\t[Initial ValidationJaccard: {2:.4f}]'
-                  '\t[Initial ValidationDice: {3:.4f}]'.format(tr_loss, vl_loss, vl_jacc, vl_dice))
-            print('----------------------------------------------------------------------------------------------------'
-                  '--------------')
+    normalize = t.Normalize(mean=[0.445, 0.287, 0.190], std=[0.31, 0.225, 0.168])
+    im_transform = t.Compose([t.ToTensor(), normalize])
 
-        for step, (images, labels) in enumerate(tLoader):
-            model.train(True)
-            if args.cuda:
-                images = images.cuda()
-                labels = labels.cuda()
-                criterion = criterion.cuda()
+    # Use  the following code fo co_transformations e.g. random rotation or random flip etc.
+    # co_transformer = cot.Compose([cot.RandomRotate(45)])
+    print(CONFIG.ROOT)
+    print(CONFIG.LABELS)
+    dataset = GIANA(CONFIG.ROOT, CONFIG.LABELS, input_size=(CONFIG.IMAGE.SIZE.WIDTH, CONFIG.IMAGE.SIZE.HEIGHT) ,train=True, transform=im_transform, co_transform=None, target_transform=t.ToLabel(), train_split=0.8)
+    #train_data_loader = data.DataLoader(dsetTrain, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
-            inputs = Variable(images)
-            targets = Variable(labels)
+    #dsetVal = GIANA(args.imgdir, args.gtdir, train=False, transform=im_transform, co_transform=None,
+                    #target_transform=t.ToLabel())
+    #val_data_loader = data.DataLoader(dsetVal, batch_size=args.batch_size, shuffle=False,
+                                      #num_workers=args.num_workers)
 
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
+
+    # DataLoader
+    loader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=CONFIG.BATCH_SIZE,
+        num_workers=CONFIG.NUM_WORKERS,
+        shuffle=True,
+    )
+    loader_iter = iter(loader)
+
+    # Model
+    model = DeepLabV3(n_classes=CONFIG.N_CLASSES, n_blocks=[3, 4, 23, 3], pyramids=[6, 12, 18])
+    #state_dict = torch.load(CONFIG.INIT_MODEL)
+    #model.load_state_dict(state_dict, strict=False)  # Skip "aspp" layer
+    model = nn.DataParallel(model)
+    if cuda:
+        model.cuda()
+
+    # Optimizer
+    optimizer = {
+        'sgd':
+            torch.optim.SGD(
+                # cf lr_mult and decay_mult in train.prototxt
+                params=[{
+                    'params': get_lr_params(model.module, key='1x'),
+                    'lr': CONFIG.LR,
+                    'weight_decay': CONFIG.WEIGHT_DECAY
+                }, {
+                    'params': get_lr_params(model.module, key='10x'),
+                    'lr': 10 * CONFIG.LR,
+                    'weight_decay': CONFIG.WEIGHT_DECAY
+                }, {
+                    'params': get_lr_params(model.module, key='20x'),
+                    'lr': 20 * CONFIG.LR,
+                    'weight_decay': 0.0
+                }]
+            ),
+    }.get(CONFIG.OPTIMIZER)
+
+    # Loss definition
+    criterion = CrossEntropyLoss2d(ignore_index=CONFIG.IGNORE_LABEL)
+    if cuda:
+        criterion.cuda()
+
+    # TensorBoard Logger
+    writer = SummaryWriter(CONFIG.LOG_DIR)
+    #loss_meter = MovingAverageValueMeter(20)
+
+    model.train()
+    model.module.scale.freeze_bn()
+
+    for iteration in tqdm(
+        range(1, CONFIG.ITER_MAX + 1),
+        total=CONFIG.ITER_MAX,
+        leave=False,
+        dynamic_ncols=True,
+    ):
+
+        # Set a learning rate
+        poly_lr_scheduler(
+            optimizer=optimizer,
+            init_lr=CONFIG.LR,
+            iter=iteration - 1,
+            lr_decay_iter=CONFIG.LR_DECAY,
+            max_iter=CONFIG.ITER_MAX,
+            power=CONFIG.POLY_POWER,
+        )
+
+        # Clear gradients (ready to accumulate)
+        optimizer.zero_grad()
+
+        iter_loss = 0
+        for i in range(1, CONFIG.ITER_SIZE + 1):
+            try:
+                data, target = next(loader_iter)
+            except:
+                loader_iter = iter(loader)
+                data, target = next(loader_iter)
+
+            # Image
+            data = data.cuda() if cuda else data
+            #data = Variable(data)
+
+            # Propagate forward
+            outputs = model(data)
+
+            # Loss
+            loss = 0
+            for output in outputs:
+                # Resize target for {100%, 75%, 50%, Max} outputs
+                target_ = resize_target(target, output.size(2))
+                target_ = target_.cuda() if cuda else target_
+                # target_ = Variable(target_)
+                # Compute crossentropy loss
+                loss += criterion(output, target_)
+
+            # Backpropagate (just compute gradients wrt the loss)
+            loss /= float(CONFIG.ITER_SIZE)
             loss.backward()
-            optimizer.step()
 
-            tr_losses.update(loss.data.cpu().numpy())
+            iter_loss += loss.data[0]
 
-        if epoch % args.log_step == 0:
-            vl_loss, vl_jacc, vl_dice = evaluate(args, model, criterion, vLoader)
-            print('[Epoch: {0:02}/{1:02}]'
-                  '\t[TrainLoss: {2:.4f}]'
-                  '\t[ValidationLoss: {3:.4f}]'
-                  '\t[ValidationJaccard: {4:.4f}]'
-                  '\t[ValidationDice: {5:.4f}]'.format(epoch, args.num_epochs, tr_losses.avg, vl_loss, vl_jacc,
-                                                       vl_dice)),
+       # loss_meter.add(iter_loss)
 
-            filename = "weights/{0}-{1:02}.pth".format(args.model, epoch)
-            torch.save(model.state_dict(), filename)
-            print('  [Snapshot]')
-        else:
-            vl_loss, vl_jacc, vl_dice = evaluate(args, model, criterion, vLoader)
-            print('[Epoch: {0:02}/{1:02}]'
-                  '\t[TrainLoss: {2:.4f}]'
-                  '\t[ValidationLoss: {3:.4f}]'
-                  '\t[ValidationJaccard: {4:.4f}]'
-                  '\t[ValidationDice: {5:.4f}]'.format(epoch, args.num_epochs, tr_losses.avg, vl_loss, vl_jacc,
-                                                       vl_dice))
+        # Update weights with accumulated gradients
+        optimizer.step()
 
-        # --- Update the loss curves ---
-        win = board.loss_curves([tr_losses.avg, vl_loss], epoch, win=win)
+        # TensorBoard
+        if iteration % CONFIG.ITER_TF == 0:
+            #writer.add_scalar('train_loss', loss_meter.value()[0], iteration)
+            for i, o in enumerate(optimizer.param_groups):
+                writer.add_scalar('train_lr_group{}'.format(i), o['lr'], iteration)
+            for name, param in model.named_parameters():
+                name = name.replace('.', '/')
+                writer.add_histogram(name, param, iteration, bins="auto")
+                if param.requires_grad:
+                    writer.add_histogram(name + '/grad', param.grad, iteration, bins="auto")
 
+        # Save a model
+        if iteration % CONFIG.ITER_SNAP == 0:
+            torch.save(
+                model.module.state_dict(),
+                osp.join(CONFIG.SAVE_DIR, 'checkpoint_{}.pth'.format(iteration)),
+            )
 
-def evaluate(args, model, criterion, val_loader):
-    model.eval()
-    losses = AverageMeter()
-    jaccars = AverageMeter()
-    dices = AverageMeter()
-    eva = Evaluation()
+        # Save a model
+        if iteration % 100 == 0:
+            torch.save(
+                model.module.state_dict(),
+                osp.join(CONFIG.SAVE_DIR, 'checkpoint_current.pth'),
+            )
 
-    for i, (images, labels) in enumerate(val_loader):
-        if args.cuda:
-            images = images.cuda()
-            labels = labels.cuda()
-            criterion = criterion.cuda()
-
-        # images = Variable(images)
-        # labels = Variable(labels)
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        losses.update(loss.data.cpu().numpy())
-
-        jacc_index = eva.jaccard_similarity_coefficient(outputs.cpu().data.numpy().squeeze(),
-                                                        labels.cpu().data.numpy())
-        dice_index = eva.dice_coefficient(outputs.cpu().data.numpy().squeeze(),
-                                                        labels.cpu().data.numpy())
-        jaccars.update(jacc_index)
-        dices.update(dice_index)
-
-    return losses.avg, jaccars.avg, dices.avg
-
-
-def eval(args, model):
-    if not os.path.exists(args.savedir):
-        os.makedirs(args.savedir)
-
-    listImgFiles = [k.split('/')[-1].split('.')[0] for k in glob.glob(os.path.join(args.imgdir, '*.bmp'))]
-    for currFile in tqdm(listImgFiles):
-        img = Image.open(os.path.join(args.imgdir, currFile + '.bmp'))
-        img = t.ToTensor()(img)
-        if args.cuda:
-            img.cuda()
-        output = model(Variable(img, volatile=True).unsqueeze(0))
-        output = t.ToPILImage()(output[0].cpu().data)
-        newfilename = os.path.join(args.savedir, currFile + '_' + args.model + '.bmp')
-        output.save(newfilename, 'BMP')
-
-def make_model(args):
-    Net = None
-    if args.model == 'deeplabv3':
-        Net = DeepLabV3
-    # if args.model == 'unet':
-    #     Net = UNet
-    # elif args.model == 'unet_res':
-    #     Net = UNetRes
-    # elif args.model == 'fcn32':
-    #     Net = FCN32
-    # elif args.model == 'fcn16':
-    #     Net = FCN16
-    # elif args.model == 'fcn8':
-    #     Net = FCN8
-    # elif args.model == 'pspnet':
-    #     Net = PSPNet
-    # elif args.model == 'segnet':
-    #     Net = SegNet
-    # elif args.model == 'fusionnet':
-    #     Net = FusionNet
-    # elif args.model == 'duc':
-    #     Net = ResentDUC
-    # elif args.model == 'duchdc':
-    #     Net = ResentDUCHDC
-    # elif args.model == 'gcn':
-    #     Net = ResnetGCN
-    # elif args.model == 'linknet':
-    #     Net = LinkNet
-
-    assert Net is not None, 'model {args.model} is not available'
-
-    model = Net(1)
-
-    if args.cuda:
-        nGPUs = torch.cuda.device_count()
-        model = torch.nn.DataParallel(model, device_ids=range(nGPUs)).cuda()
-        model = model.cuda()
-        if args.state:
-            model.load_state_dict(torch.load('weights/' + args.state))
-
-    # In case to use weights trained on multiple GPUs in CPU mode
-    else:
-        checkpoint = torch.load('./weights/' + args.state)
-        state_dict = checkpoint['state_dict']
-        from collections import OrderedDict
-        new_state_dict = OrderedDict()
-        for k, v in state_dict.items():
-            name = k[0:9] + k[16:]  # remove `module.`
-            if k[0] == 'f':
-                new_state_dict[name] = v
-            else:
-                new_state_dict[k] = v
-
-        model.load_state_dict(new_state_dict)
-        model = model.cpu()
-    return model
-
-
-def main(args):
-    torch.backends.cudnn.enabled = False
-
-    model = make_model(args)
-    if args.mode == 'train':
-        train(args, model)
-    elif args.mode == 'eval':
-        eval(args, model)
+    torch.save(
+        model.module.state_dict(),
+        osp.join(CONFIG.SAVE_DIR, 'checkpoint_final.pth'),
+    )
 
 
 if __name__ == '__main__':
-
-    args = get_arguments(sys.argv[1:])
-
-    if args.mode == 'crossval':
-        params = list(itertools.product(args.batch_size, args.optimizer, args.momentum, args.lr))
-        args.batch_size, args.optimizer, args.momentum, args.lr = params[args.settings_id]
-
-    main(args)
+    main()
